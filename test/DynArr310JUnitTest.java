166dc63f9a6d1ba4e2615f7b3df0831101c27f6a
skommi2 <60101559+skommi2@users.noreply.github.com>
2020-02-09 18:29:54 -0500

    initial

---
+++ test/DynArr310JUnitTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+ 
+/**
+ *
+ * @author Srinivas
+ */
+public class DynArr310JUnitTest {
+
+    public DynArr310JUnitTest() {
+    }
+
+    @BeforeAll
+    public static void setUpClass() {
+    }
+
+    @AfterAll
+    public static void tearDownClass() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @AfterEach
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void hello()
+    {
+	//create a dynamic array of integers
+	DynArr310<Integer> nums = new DynArr310<>();
+	if ((nums.size() == 0) && (nums.capacity() == 2)){
+		System.out.println("Yay 1");
+	}   
+    }
+
+
+}
